623be25eb55e595d210a8936c3dee167c8009514
Albert Kim <albertkim91@gmail.com>
2022-07-11 21:02:58 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import gsap from "gsap";
+
+const Wrapper = styled.div``;
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <p>hello world</p>
+    </Wrapper>
+  );
+}
